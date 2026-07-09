f7e002678a6edfcee68ef68669143b5de3316a0e
shivrajkachare <52147978+shivrajkachare@users.noreply.github.com>
2026-07-09 19:35:55 +0530

    Update UserRepository.java
    git change

--- Desktop/Shiv_Java/Microservices/UserService/src/main/java/com/UserService/UserService/UserRepository.java
+++ Desktop/Shiv_Java/Microservices/UserService/src/main/java/com/UserService/UserService/UserRepository.java
@@ -2,7 +2,7 @@ package com.UserService.UserService;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-//Another My Local change
+//Another My Git change
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
